23e072f251d2f07ba701b0f07394fb8cb0702fce
wathika-eng <wathika02@gmail.com>
2024-01-21 22:14:26 +0300

    multiple uploads

--- Main/views.py
+++ Main/views.py
@@ -6,25 +6,22 @@ from Main.models import Post
 
 # Create your views here.
 def index(request):
-    posts=Post.objects.all().order_by("-created_at")
-    context={
-        'posts':posts
-    }
-    return render(request,'index.html',context)
+    posts = Post.objects.all().order_by("-created_at")
+    context = {"posts": posts}
+    return render(request, "index.html", context)
 
 
 def upload_files(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save()  # Save the post instance
 
-            # Process each image uploaded
-            for image in request.FILES.getlist('images'):
+            for image in request.FILES.getlist("images"):
                 post.images.create(image=image)
 
-            return redirect('index')
+            return redirect("index")
     else:
         form = PostForm()
 
-    return render(request,"upload_file.html",{'form':form})
+    return render(request, "upload_file.html", {"form": form})
